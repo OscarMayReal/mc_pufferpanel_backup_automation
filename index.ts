aa52689290ce136f277dec909f09e1d0e9ffc31e
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2025-09-05 19:28:08 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const { chromium } = pkg
 const timeout = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 async function downloadWorld() {
-    const browser = await chromium.launch({ headless: false })
+    const browser = await chromium.launch({ headless: true })
     const context = await browser.newContext()
     const page = await context.newPage()
 
